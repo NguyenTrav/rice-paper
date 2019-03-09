0b83b318293f7571a0cf60f97677586dd74a213a
Travis <NguyenTrav@gmail.com>
2019-03-09 12:48:01 -0500

    added scrolling to nav links

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,43 @@
-if (jQuery)
-{
-	alert("loaded");
-}
-else
-{
-	alert("not loaded");
-}
+$(function() 
+{ 
+	$("#story-scroll").on('click', function() 
+		{ 
+			var position = $(".section-story").offset().top; 
+
+			$("HTML, BODY").animate({ scrollTop: position 
+		}, 1500); 
+	});
+});
+
+$(function() 
+{ 
+	$("#menu-scroll").on('click', function() 
+		{ 
+			var position = $(".section-menu").offset().top; 
+
+			$("HTML, BODY").animate({ scrollTop: position 
+		}, 1500); 
+	});
+});
+
+$(function() 
+{ 
+	$("#awards-scroll").on('click', function() 
+		{ 
+			var position = $(".section-awards").offset().top; 
+
+			$("HTML, BODY").animate({ scrollTop: position 
+		}, 1500); 
+	});
+});
+
+$(function() 
+{ 
+	$("#contact-scroll").on('click', function() 
+		{ 
+			var position = $(".section-contact").offset().top; 
+
+			$("HTML, BODY").animate({ scrollTop: position 
+		}, 1500); 
+	});
+});
